f6e5b0cfda575a6dc26129b8b86e3e8f553992d4
castilho <guilherme.castilhot2c18@gmail.com>
2024-03-18 16:41:42 -0300

    chart pie

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,32 @@
 import Resume from "../../components/Resume";
 
+import React from "react";
+import { Chart } from "react-google-charts";
+
 const Home = () => {
+  const data = [
+    ["Task", "Hours per Day"],
+    ["Work", 11],
+    ["Eat", 2],
+    ["Commute", 2],
+    ["Watch TV", 2],
+    ["Sleep", 7],
+  ];
+
+  const options = {
+    title: "Despesas por categoria: ",
+  };
+
   return (
     <div>
       <Resume />
+      <Chart
+        chartType="PieChart"
+        data={data}
+        options={options}
+        width={"100%"}
+        height={"400px"}
+      />
     </div>
   );
 };
