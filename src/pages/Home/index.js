6940ecd41a2cb7a92f151a17cb4f370faf566c48
castilho <guilherme.castilhot2c18@gmail.com>
2024-03-19 02:07:00 -0300

    novos componentes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,8 @@
-import Resume from "../../components/Resume";
+import React, { useEffect, useState } from "react";
 
-import React from "react";
+import Resume from "../../components/Resume";
+import Form from "../../components/Form";
+
 import { Chart } from "react-google-charts";
 
 const Home = () => {
@@ -27,14 +29,56 @@ const Home = () => {
   const optionsBalance = {
     chart: {
       title: "Balanço Mensal",
-      subtitle: "Sales, Expenses, and Profit: 2014-2017",
+      subtitle: "receitas e despesas mensal",
     },
+  };
+
+  const dataT = localStorage.getItem("transactions");
+  const [transactionsList, setTransactionsList] = useState(
+    dataT ? JSON.parse(dataT) : []
+  );
+  const [income, setIncome] = useState(0);
+  const [expense, setExpense] = useState(0);
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    const amountExpense = transactionsList
+      .filter((item) => item.expense)
+      .map((transaction) => Number(transaction.amount));
+
+    const amountIncome = transactionsList
+      .filter((item) => !item.expense)
+      .map((transaction) => Number(transaction.amount));
+
+    const expense = amountExpense.reduce((acc, cur) => acc + cur, 0).toFixed(2);
+    const income = amountIncome.reduce((acc, cur) => acc + cur, 0).toFixed(2);
+
+    const total = Math.abs(income - expense).toFixed(2);
+
+    setIncome(`R$ ${income}`);
+    setExpense(`R$ ${expense}`);
+    setTotal(`${Number(income) < Number(expense) ? "-" : ""}R$ ${total}`);
+  }, [transactionsList]);
+
+  const handleAdd = (transaction) => {
+    const newArrayTransactions = [...transactionsList, transaction];
+
+    setTransactionsList(newArrayTransactions);
+
+    localStorage.setItem("transactions", JSON.stringify(newArrayTransactions));
   };
 
   return (
     <div>
-      <Resume />
-      <Chart
+      <Resume income={income} expense={expense} total={total} />
+
+      <Form
+        handleAdd={handleAdd}
+        transactionsList={transactionsList}
+        setTransactionsList={setTransactionsList}
+      />
+
+      {/* <Chart
         chartType="PieChart"
         data={data}
         options={options}
@@ -47,7 +91,7 @@ const Home = () => {
         height="400px"
         data={dataBalance}
         options={optionsBalance}
-      />
+      /> */}
     </div>
   );
 };
