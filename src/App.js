baefaf2ed6a65b632d4fd278d11a201f975a6e65
castilho <guilherme.castilhot2c18@gmail.com>
2024-03-19 11:42:07 -0300

    global styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import RoutesApp from "./routes";
+import Global from "./styles/global";
 
 function App() {
   return (
     <div className="App">
       <RoutesApp />
+
+      <Global />
     </div>
   );
 }
