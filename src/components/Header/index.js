274a51e7f55ccc3af67853d427fab83cbbf3379d
castilho <guilherme.castilhot2c18@gmail.com>
2024-03-19 15:10:45 -0300

    melhorias header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,7 +3,14 @@ import * as C from "./styles";
 const Header = () => {
   return (
     <C.Header>
+      <C.Box>
+        <C.ContainerLink to="/">Home</C.ContainerLink>
+        <C.ContainerLink to="/dashboard">Dashboard</C.ContainerLink>
+      </C.Box>
+
+      <C.Box>
         <h1>Abril</h1>
+      </C.Box>
     </C.Header>
   );
 };
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,10 +1,30 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 export const Header = styled.header`
   display: flex;
   align-items: center;
-  justify-content: space-around;
+  justify-content: space-beetween;
   width: 100%;
   height: 60px;
-  background-color: #ff9a3c;
+  background-color: #007cb9;
 `;
+
+export const Box = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 50%;
+  height: 60px;
+  gap: 5px;
+`
+
+export const ContainerLink = styled(Link)`
+  text-decoration: none;
+  background-color: #eeeeee;
+  color: #060608;
+  padding: 10px;
+  border-radius: 2px
+`;
+
+
